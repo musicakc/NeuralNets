cc018ba32106eaf9b6cc796852773791e497c7aa
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2017-10-27 19:27:49 -0400

    modified two layer to implement the three layer nn

--- threelayernn.py
+++ threelayernn.py
@@ -21,7 +21,7 @@ def nonlin(x,deriv=False):
 x = np.array([[0,0,1], [0,1,1],[1,0,1],[1,1,1]])
 
 #output y_(4*1)
-y = np.array([[0,0,1,1]]).T #transpose the output array
+y = np.array([[0,1,1,0]]).T #transpose the output array
 
 #seed random numbers
 np.random.seed(1)
@@ -30,22 +30,41 @@ np.random.seed(1)
 Initialise weights randomly with mean 0, 
 of dimension [3*1] for 3 inputs and 1 ouput
 '''
-syn0 = 2 * np.random.random((3,1)) - 1
+syn0 = 2 * np.random.random((3, 4)) - 1
+syn1 = 2 * np.random.random((4, 1)) - 1
 
 #iterate multiple times to optimise our neural network
-for i in range(10000):
+for i in range(80000):
+
 	#forward propogation
 	l0 = x
-	#hidden layer l1_(4*1) = [l0_(4*3) dot syn0_(3*1)]
+	#hidden layer l1_(4*4) = [l0_(4*3) dot syn0_(3*4)]
 	l1 = nonlin(np.dot(l0,syn0))
+	#hidden layer l2_(4*1) = [l1_(4*4) dot syn1_(4*1)]
+	l2 = nonlin(np.dot(l1,syn1))
 
-	#calculate error between output and l1
-	l1_error = y - l1
+	#calculate error between output and l2
+	l2_error = y - l2
+
+	if ((i % 10000) == 0):
+		print ("Error:" + str(np.mean(np.abs(l2_error))))
+
+	#slope of l2 * error to reduce error of high confidence predictions
+	l2_delta = l2_error * nonlin(l2,True)
+
+	'''
+	calculate confidence weighted error by calculating how much
+	each node value contributed to the error in l2
+	'''
+	l1_error = l2_delta.dot(syn1.T)
 
 	#slope of l1 * error to reduce error of high confidence predictions
 	l1_delta = l1_error * nonlin(l1,True)
 
+	#update weights using l2_delta
+	syn1 += np.dot(l1.T, l2_delta)
 	#update weights using l1_delta
 	syn0 += np.dot(l0.T, l1_delta)
 
-print (l1)
+
+#print (l1)
